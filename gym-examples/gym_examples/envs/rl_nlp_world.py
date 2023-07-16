9671bddc7f17f7faf7cbde9d9765897aba70ba0a
tirthankarCU <timi5773@colorado.edu>
2023-07-15 21:18:42 -0600

    fix again

--- gym-examples/gym_examples/envs/rl_nlp_world.py
+++ gym-examples/gym_examples/envs/rl_nlp_world.py
@@ -165,7 +165,7 @@ class RlNlpWorld(gym.Env):
             reward=put(BOXTYPE.SMALL)
 
         '''Extra reward for following instructions'''
-        reward= 1 if action == self.exp_actions[self.nlp_index] else reward
+        reward= 1 if action == self.exp_actions[self.nlp_index] and reward != -2 else reward
         vga.carry_indicator=self.carry
         self._visual=vga.drawAgain()
         if action == self.exp_actions[self.nlp_index] and self.nlp_index<len(self.instructions)-1:
